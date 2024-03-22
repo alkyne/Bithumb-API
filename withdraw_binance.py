392808d12320117fa0caa5053689ee859788f472
rocketman <9082457@naver.com>
2024-03-22 10:25:06 +0900

    Add sol withdraw

--- withdraw_binance.py
+++ withdraw_binance.py
@@ -16,6 +16,11 @@ bybit_address = {
     "exchange_name": "BYBIT"
 }
 
+upbit_sol_address = {
+    "address": "DajThCX3PGn17SUvR7MRMjCNmNpk6m7qpTBbY13QLN9P",
+    "exchange_name": "UPBIT"
+}
+
 def bithumb_api_query(endpoint, payload):
     api = XCoinAPI(API_KEY, API_SECRET)
     rgParams = {
@@ -27,45 +32,58 @@ def bithumb_api_query(endpoint, payload):
     result = api.xcoinApiCall(rgParams['endpoint'], rgParams)
     return result
 
-def get_available_usdt():
-    
+def get_available(currency):
+    currency = currency.lower() # for dictionary key
     endpoint = '/info/balance'
-    payload =  { "currency": "USDT" }
+    payload =  { "currency": currency }
     response = bithumb_api_query(endpoint, payload)
-    available_usdt = response['data']['available_usdt']
+    available_usdt = response['data'][f'available_{currency}']
     return available_usdt
 
 
-def withdraw_usdt(units, address):
+def withdraw_usdt(units, address, currency="USDT"):
     endpoint = '/trade/btc_withdrawal'
     payload = {
     "cust_type_cd": "01",
     "units": units,
     "address": address["address"],
-    "currency": "USDT",
+    "currency": currency,
     "ko_name": "최동민",
     "en_name": "DONGMIN CHOI",
     "exchange_name": address["exchange_name"]
     }
     response = bithumb_api_query(endpoint, payload)
     print(response)
-    currency = "USDT"
     if response['status'] == "0000":
         print(f"withdraw {units} {currency} success !!")
     return response
 
-available_usdt = get_available_usdt()
+
+while True:
+    currency = "SOL"
+    available_sol = get_available(currency)
+    print(f"now available currency: {available_sol}")
+    sleep(5)
+    if float(available_sol) > 61:
+        withdraw_usdt(available_sol, upbit_sol_address, currency)
+        sys.exit(0)
+
+
+'''
+available_usdt = get_available("USDT")
 print(f"now available usdt: {available_usdt}")
 
 input()
-withdraw_usdt(available_usdt, bybit_address)
+# withdraw_usdt(available_usdt, binance_adddress)
 
-# while True:
-#     usdt_balance = get_available_usdt()
-#     sleep(1)
+while True:
+    usdt_balance = get_available("USDT")
+    sleep(1)
 
-#     print(f"now available usdt balance: {usdt_balance}")
+    print(f"now available usdt balance: {usdt_balance}")
 
-#     if float(usdt_balance) > 21000:
-#         withdraw_usdt(usdt_balance, addr, exchange_name)
-#         sys.exit(0)
+    if float(usdt_balance) > 20000:
+        withdraw_usdt(available_usdt, binance_adddress)
+        sys.exit(0)
+
+'''
